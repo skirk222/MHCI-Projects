c6715ac6beff1ffe42434fa6f256315cf5666aef
sdkirkman <91206410+skirk222@users.noreply.github.com>
2021-10-29 02:17:11 -0400

    (javascript 2)

--- Assignment 6A/main.js
+++ Assignment 6A/main.js
@@ -1,11 +1,16 @@
-function updateColor() {
-    var Harness_Image = document.getElementById ('original')
-    if Harness_Image.src.match("harness.png") {
-        Harness_Image.src = "orangeharness.png";
-        Harness_Image.src = "blueharness.png";
-        Harness_Image.src= "purpleharness.png";
-    }
-    else {
-        Image_Id.src = "harness.png";
-    }
-}
+// function updateColor() {
+//     var Harness_Image = document.getElementById ('original')
+//     if Harness_Image.src.match("harness.png") {
+//         Harness_Image.src = "orangeharness.png";
+//         Harness_Image.src = "blueharness.png";
+//         Harness_Image.src= "purpleharness.png";
+//     }
+//     else {
+//         Image_Id.src = "harness.png";
+//     }
+// }
+// function updateColorOrange() {
+//     var updateColorOrange = document.getElementById("original")
+//     updateColorOrange.src = 'orangeharness.png';
+
+// }
