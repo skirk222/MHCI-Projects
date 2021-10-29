ee9e2e1bd882331e8ea62b6568cff0cb3a609f63
sdkirkman <91206410+skirk222@users.noreply.github.com>
2021-10-28 23:59:27 -0400

    (commit js attempts 1)

---
+++ Assignment 6A/main.js
@@ -0,0 +1,11 @@
+function updateColor() {
+    var Harness_Image = document.getElementById ('original')
+    if (Harness_Image.src.match("harness.png")) {
+        Harness_Image.src = "orangeharness.png";
+        Harness_Image.src = "blueharness.png";
+        Harness_Image.src= "purpleharness.png";
+    }
+    else {
+        Image_Id.src = "harness.png";
+    }
+}
